aa63ff2f56ec1d4dabbe56fd3ba218ed5bfcabd2
dv-justin <1004kjs_@naver.com>
2025-03-05 09:11:04 +0900

    feat: 알림 기능 개발 중

--- app/(tabs)/alarm/index.tsx
+++ app/(tabs)/alarm/index.tsx
@@ -1,31 +1,106 @@
 import { theme } from "@/constants/Theme";
+import { useEffect, useState } from "react";
 import { View, StyleSheet, Text, Pressable, Image } from "react-native";
 import {
   responsiveHeight,
   responsiveWidth,
 } from "react-native-responsive-dimensions";
+import io from "socket.io-client";
+
+interface Notification {
+  id: number;
+  contents: string;
+  category: string;
+}
 
 export default function AlarmScreen() {
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+  useEffect(() => {
+    const socket = io("http://localhost:4001/notifications", {
+      transports: ["websocket"],
+    });
+
+    socket.on("notifications", (data) => {
+      console.log("Received message:", data);
+      setNotifications(data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from the server");
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const getBadgeStyle = (category: string) => {
+    let backgroundColor;
+    switch (category) {
+      case "결제":
+        backgroundColor = theme.colors.payment;
+        break;
+      case "만남":
+        backgroundColor = theme.colors.primary;
+        break;
+      case "매칭":
+        backgroundColor = theme.colors.matching;
+        break;
+      default:
+        backgroundColor = theme.colors.primary;
+    }
+    return {
+      ...styles.badge,
+      backgroundColor,
+    };
+  };
+
+  const getAlarmItemStyle = (category: string) => {
+    let backgroundColor;
+    switch (category) {
+      case "결제":
+        backgroundColor = theme.colors.paymentRgb;
+        break;
+      case "만남":
+        backgroundColor = theme.colors.primaryRgb;
+        break;
+      case "매칭":
+        backgroundColor = theme.colors.matchingRgb;
+        break;
+      default:
+        backgroundColor = theme.colors.primary;
+    }
+    return {
+      ...styles.alarmItem,
+      backgroundColor,
+    };
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.selectBar}>
         <View></View>
-        <Text style={styles.selectBarTitle}>결제</Text>
+        <Text style={styles.selectBarTitle}>전체</Text>
         <Image
           style={styles.nextImage}
           source={require("@/assets/images/next.png")}
         />
       </View>
-      <View style={styles.alarmGroup}>
-        <View style={styles.alarmItem}>
-          <View style={styles.badge}>
-            <Text style={styles.badgeTitle}>결제</Text>
+      {notifications?.map((notification, index) => (
+        <View style={styles.alarmGroup} key={index}>
+          <View
+            style={[
+              styles.alarmItem,
+              getAlarmItemStyle(notification?.category),
+            ]}
+          >
+            <View style={[styles.badge, getBadgeStyle(notification?.category)]}>
+              <Text style={styles.badgeTitle}>{notification?.category}</Text>
+            </View>
+            <Text style={styles.alarmTitle}>{notification?.contents}</Text>
           </View>
-          <Text style={styles.alarmTitle}>
-            김진수님과 연결되셨습니다 매니저 연락을 기다려주세요
-          </Text>
         </View>
-      </View>
+      ))}
     </View>
   );
 }
@@ -45,7 +120,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
     alignItems: "center",
     borderRadius: 10,
-    backgroundColor: theme.colors.primary,
+    backgroundColor: theme.colors.black,
   },
   nextImage: {
     width: 12,
